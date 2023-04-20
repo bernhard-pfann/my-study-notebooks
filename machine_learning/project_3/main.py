b1ebc5bb91bfb8bb7e51f8f2f7a539250f213310
bernhard <bernhard.pfann@rbinternational.com>
2023-04-20 08:14:34 +0200

    added em elgo

--- machine_learning/project_3/main.py
+++ machine_learning/project_3/main.py
@@ -7,3 +7,8 @@ import em
 X = np.loadtxt("toy_data.txt")
 
 # TODO: Your code here
+K=1
+for seed in range(1,5):
+    mixture, post = common.init(X=X, K=K, seed=seed)
+    mixture, post, cost = kmeans.run(X=X, mixture=mixture, post=post)
+    print(seed, cost)
--- machine_learning/project_3/naive_em.py
+++ machine_learning/project_3/naive_em.py
@@ -17,7 +17,33 @@ def estep(X: np.ndarray, mixture: GaussianMixture) -> Tuple[np.ndarray, float]:
             for all components for all examples
         float: log-likelihood of the assignment
     """
-    raise NotImplementedError
+
+    from scipy.stats import multivariate_normal
+    
+    k, d = mixture.mu.shape
+    n = len(X)
+    posteriors_all = np.zeros((n, k))
+
+    # Iterate over observations in X
+    for i in range(n):
+        normalization = 0
+        posteriors = np.zeros(k)
+
+        # Iterate over k components
+        for j in range(k):
+            cov_matrix = np.identity(d) * mixture.var[j]
+            likelihood = multivariate_normal.pdf(X[i], mean=mixture.mu[j], cov=cov_matrix)
+            posterior = mixture.p[j] * likelihood
+            posteriors[j] = posterior        
+    
+        posteriors_all[i] = posteriors
+
+    normalization = posteriors_all.sum(axis=1)
+    posteriors_all = posteriors_all / normalization.reshape(-1,1)
+
+    log_likelihood = np.sum(np.log(normalization))
+
+    return posteriors_all, log_likelihood
 
 
 def mstep(X: np.ndarray, post: np.ndarray) -> GaussianMixture:
@@ -32,7 +58,29 @@ def mstep(X: np.ndarray, post: np.ndarray) -> GaussianMixture:
     Returns:
         GaussianMixture: the new gaussian mixture
     """
-    raise NotImplementedError
+    dim = X.shape[1]
+    k = post.shape[1]
+
+    hat_mu = np.zeros((k, dim))
+    hat_var = np.zeros(k)
+
+    hat_n = post.sum(axis=0)
+    hat_p = hat_n / hat_n.sum()
+
+    for j in range(k):
+        # Estimate mu
+        hat_mu_j = X*post[:,j].reshape(-1,1)
+        hat_mu_j = hat_mu_j.sum(axis=0)/hat_n[j]
+        hat_mu[j] = hat_mu_j
+ 
+        # Estimate variance
+        denom_j = hat_n[j] * dim
+        dist_j = np.sum(np.square(X - hat_mu[j]), axis=1)
+        hat_var_j = np.sum(post[:,j] * dist_j) / denom_j
+        hat_var[j] = hat_var_j
+
+    model = GaussianMixture(mu=hat_mu, var=hat_var, p=hat_p)
+    return model
 
 
 def run(X: np.ndarray, mixture: GaussianMixture,
@@ -50,4 +98,21 @@ def run(X: np.ndarray, mixture: GaussianMixture,
             for all components for all examples
         float: log-likelihood of the current assignment
     """
-    raise NotImplementedError
+    condition = True
+    
+    while condition:
+
+        old_log_lh = new_log_lh
+
+        # Update steps
+        post, new_log_lh = estep(X=X, mixture=mixture)
+        mixture = mstep(X=X, post=post)
+        
+        # Evaluate while condition  
+        condition = (new_log_lh - old_log_lh) > 1e-6 * np.abs(new_log_lh)
+
+    return mixture, new_log_lh
+
+
+
+
